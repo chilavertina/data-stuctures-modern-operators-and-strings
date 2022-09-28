800229b521d4acc9672f79fa63b3061ec716eb38
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-09-28 18:52:17 +0200

    primer rest operatora

--- script.js
+++ script.js
@@ -43,6 +43,11 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 // 1. Destruktuiranje nizova i objekata uz pomoc REST operatora
@@ -77,6 +82,9 @@ add(8, 2, 5, 3, 2, 1, 4);
 
 const x = [23, 5, 7];
 add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
 
 /*
 // Spread operator
