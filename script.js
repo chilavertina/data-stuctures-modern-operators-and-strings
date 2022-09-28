a08c0b17223db54adad738d1bac298d144ddf4c5
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-09-28 19:55:44 +0200

    nullish assignment operator

--- script.js
+++ script.js
@@ -52,7 +52,8 @@ const restaurant = {
 
 const rest1 = {
   name: 'Capri',
-  numGuests: 20,
+  // numGuests: 20,
+  numGuests: 0,
 };
 
 const rest2 = {
@@ -63,8 +64,13 @@ const rest2 = {
 // OR assignment operator
 // rest1.numGuests = rest1.numGuests || 10;
 // rest2.numGuests = rest2.numGuests || 10;
-rest1.numGuests ||= 10;
-rest2.numGuests ||= 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
 console.log(rest1);
 console.log(rest2);
 /*
