0184908702e296264e9055583d2f249cfc1446a5
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-09-28 19:11:11 +0200

    primer short circuiting OR operatora

--- script.js
+++ script.js
@@ -58,6 +58,13 @@ console.log(undefined || null);
 
 console.log(undefined || 0 || '' || 'Hello' || 23 || null); //ovde je rezultat 'Hello' jer je prvi true element, posto su undefined, 0 i '' false
 
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
 /*
 // 1. Destruktuiranje nizova i objekata uz pomoc REST operatora
 
