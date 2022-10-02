8791f406b8b056964c846a5f565cd389e341d9d3
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-10-02 18:43:15 +0200

    resenje coding challenge

--- codingchallenge.js
+++ codingchallenge.js
@@ -39,6 +39,20 @@ const game = {
   },
 };
 
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
 //CODING CHALLENGE 1
 /* 
 const [players1, players2] = game.players;
@@ -72,7 +86,7 @@ team1 < team2 && console.log(`Team1 is a winner!`);
 */
 
 // CODING CHALLENGE 2
-
+/*
 const strelci = Object.entries(game.scored);
 console.log(strelci);
 for (const [gol, igrac] of strelci) {
@@ -93,4 +107,29 @@ console.log(`Srednja kvota je ${srednjaKvota / kvote.length}`);
 for (const [tim, kvota] of Object.entries(game.odds)) {
   const kvotaTimova = tim === 'x' ? ' nereseno' : ` pobedu ${game[tim]}`;
   console.log(`Kvota za${kvotaTimova} je: ${kvota}`);
+}
+*/
+
+//CODING CHALLENGE 3
+//1.
+let eventsArr = [...gameEvents.values()];
+console.log(eventsArr);
+const events = new Set(eventsArr);
+console.log(events);
+
+//2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+//4.
+console.log([...gameEvents.entries()]);
+for (const [min, eve] of gameEvents.entries()) {
+  const izvestaj =
+    min < 45 ? `[FIRST HALF] ${min}: ${eve}` : `[SECOND HALF] ${min}: ${eve}`;
+  console.log(izvestaj);
 }
